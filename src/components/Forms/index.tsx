287a7eb442c8ba07aff2767d7db5ad4d587ff5ac
thaismichelini <thaislmichelini@gmail.com>
2022-11-18 11:59:28 -0300

    wip: styling with sass

--- src/components/Forms/index.tsx
+++ src/components/Forms/index.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import Button from '../Button';
+import './style.scss';
 
 class Forms extends React.Component {
   render(): React.ReactNode {
     return (
-      <form>
-        <div>
+      <form className="novaTarefa">
+        <div className="inputContainer">
           <label htmlFor="tarefa">Adicione um novo estudo</label>
           <input
             type="text"
@@ -15,7 +16,7 @@ class Forms extends React.Component {
             required
           />
         </div>
-        <div>
+        <div className="inputContainer">
           <label htmlFor="tempo">Tempo</label>
           <input
             type="time"
--- src/components/List/index.tsx
+++ src/components/List/index.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import './style.scss';
 
 function List() {
   const tasks = [
@@ -16,11 +17,11 @@ function List() {
     },
   ];
   return (
-    <aside>
+    <aside className="listaTarefas">
       <h2>Estudo do dia</h2>
       <ul>
         {tasks.map((item, index) => (
-          <li key={index}>
+          <li key={index} className="item">
             <h3> {item.task}</h3>
             <span> {item.time}</span>
           </li>
