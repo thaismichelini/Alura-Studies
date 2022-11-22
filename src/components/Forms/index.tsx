3e01ef4d9866b776fa01ec10adbaaba452467ddf
thaismichelini <thaislmichelini@gmail.com>
2022-11-22 09:31:16 -0300

    wip: add state in list

--- src/components/Forms/index.tsx
+++ src/components/Forms/index.tsx
@@ -3,14 +3,26 @@ import Button from '../Button';
 import style from './Forms.module.scss';
 
 class Forms extends React.Component {
+  state = {
+    task: '',
+    time: '00:00',
+  };
+  addTask(evento: React.FormEvent<HTMLFormElement>) {
+    evento.preventDefault();
+    console.log('state: ', this.state);
+  }
   render(): React.ReactNode {
     return (
-      <form className={style.novaTarefa}>
+      <form className={style.novaTarefa} onSubmit={this.addTask.bind(this)}>
         <div className={style.inputContainer}>
           <label htmlFor="tarefa">Adicione um novo estudo</label>
           <input
             type="text"
             name="tarefa"
+            value={this.state.task}
+            onChange={(evento) =>
+              this.setState({ ...this.state, task: evento.target.value })
+            }
             id="tarefa"
             placeholder="O que você quer estudar"
             required
@@ -22,6 +34,10 @@ class Forms extends React.Component {
             type="time"
             step="1"
             name="tempo"
+            value={this.state.time}
+            onChange={(evento) =>
+              this.setState({ ...this.state, time: evento.target.value })
+            }
             id="tempo"
             min="00:00:00"
             max="01:30:00"
--- src/components/List/index.tsx
+++ src/components/List/index.tsx
@@ -1,9 +1,9 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Item from './Item';
 import style from './List.module.scss';
 
 function List() {
-  const tasks = [
+  const [tasks, setTasks] = useState([
     {
       task: 'React',
       time: '02:00:00',
@@ -16,10 +16,15 @@ function List() {
       task: 'TypeScript',
       time: '03:00:00',
     },
-  ];
+  ]);
   return (
     <aside className={style.listaTarefas}>
-      <h2>Estudo do dia</h2>
+      <h2
+        onClick={() => {
+          setTasks([...tasks, { task: 'Estudar estado', time: '05:00:00' }]);
+        }}>
+        Estudo do dia
+      </h2>
       <ul>
         {tasks.map((item, index) => (
           <Item key={index} task={item.task} time={item.time} /> // mesmo que {... item}
